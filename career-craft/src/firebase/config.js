b97ff81d1ff51febaa9674999ed4aee1a7df503f
Pari-Maurya <parimaurya2006@gmail.com>
2026-08-17 22:46:48 +0530

    updated security credentials

--- career-craft/src/firebase/config.js
+++ career-craft/src/firebase/config.js
@@ -8,14 +8,14 @@ import { getDatabase } from 'firebase/database'
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAk1SEpujakJJT0uJACzCfX2LsLH2Pi1AA",
-  authDomain: "careercraft-7c7e6.firebaseapp.com",
-  databaseURL: "https://careercraft-7c7e6-default-rtdb.firebaseio.com",
-  projectId: "careercraft-7c7e6",
-  storageBucket: "careercraft-7c7e6.firebasestorage.app",
-  messagingSenderId: "745815587862",
-  appId: "1:745815587862:web:0adfded0a33d2e48378937",
-  measurementId: "G-2XKMZMN27N"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
